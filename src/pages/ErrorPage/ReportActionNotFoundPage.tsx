de6c0b49ff82f8788e372a72603c89a47d87c3a6
Andrew Rosiclair <arosiclair@expensify.com>
2026-04-20 16:41:55 -0400

    show comment not found copy

--- src/pages/ErrorPage/ReportActionNotFoundPage.tsx
+++ src/pages/ErrorPage/ReportActionNotFoundPage.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import ScreenWrapper from '@components/ScreenWrapper';
-import Navigation from '@libs/Navigation/Navigation';
+import useResponsiveLayout from '@hooks/useResponsiveLayout';
+import useThemeStyles from '@hooks/useThemeStyles';
 
 // eslint-disable-next-line rulesdir/no-negated-variables
 function ReportActionNotFoundPage() {
+    const styles = useThemeStyles();
+    const {shouldUseNarrowLayout} = useResponsiveLayout();
+
     return (
         <ScreenWrapper testID="ReportActionNotFoundPage">
             <FullPageNotFoundView
                 shouldShow
-                onBackButtonPress={() => Navigation.goBack()}
+                subtitleKey="notFound.commentYouLookingForCannotBeFound"
+                subtitleStyle={[styles.textSupporting]}
+                shouldShowBackButton={shouldUseNarrowLayout}
+                shouldShowLink
+                linkTranslationKey="notFound.goToChatInstead"
+                subtitleKeyBelowLink="notFound.contactConcierge"
+                onLinkPress={() => {}}
+                shouldDisplaySearchRouter
+                onBackButtonPress={() => {}}
             />
         </ScreenWrapper>
     );
