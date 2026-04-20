a4f1758e5ee6a5bc16e285379bd4384cedc4051e
Andrew Rosiclair <arosiclair@expensify.com>
2026-04-20 16:42:26 -0400

    navigate back to report

--- src/pages/ErrorPage/ReportActionNotFoundPage.tsx
+++ src/pages/ErrorPage/ReportActionNotFoundPage.tsx
@@ -1,13 +1,22 @@
+import {useRoute} from '@react-navigation/core';
 import React from 'react';
 import FullPageNotFoundView from '@components/BlockingViews/FullPageNotFoundView';
 import ScreenWrapper from '@components/ScreenWrapper';
 import useResponsiveLayout from '@hooks/useResponsiveLayout';
 import useThemeStyles from '@hooks/useThemeStyles';
+import getNonEmptyStringOnyxID from '@libs/getNonEmptyStringOnyxID';
+import Navigation from '@libs/Navigation/Navigation';
+import ROUTES from '@src/ROUTES';
 
 // eslint-disable-next-line rulesdir/no-negated-variables
 function ReportActionNotFoundPage() {
     const styles = useThemeStyles();
     const {shouldUseNarrowLayout} = useResponsiveLayout();
+    const route = useRoute();
+    const routeParams = route.params as {reportID?: string; reportActionID?: string} | undefined;
+    const reportIDFromRoute = getNonEmptyStringOnyxID(routeParams?.reportID);
+
+    const goBackToReport = () => Navigation.goBack(ROUTES.REPORT_WITH_ID.getRoute(reportIDFromRoute));
 
     return (
         <ScreenWrapper testID="ReportActionNotFoundPage">
@@ -19,9 +28,9 @@ function ReportActionNotFoundPage() {
                 shouldShowLink
                 linkTranslationKey="notFound.goToChatInstead"
                 subtitleKeyBelowLink="notFound.contactConcierge"
-                onLinkPress={() => {}}
+                onLinkPress={goBackToReport}
                 shouldDisplaySearchRouter
-                onBackButtonPress={() => {}}
+                onBackButtonPress={goBackToReport}
             />
         </ScreenWrapper>
     );
