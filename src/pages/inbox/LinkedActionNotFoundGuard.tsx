013aa237aefa4877a7f84b609767714b81de8754
Andrew Rosiclair <arosiclair@expensify.com>
2026-04-22 14:56:08 -0400

    just go back when there's history

--- src/pages/inbox/LinkedActionNotFoundGuard.tsx
+++ src/pages/inbox/LinkedActionNotFoundGuard.tsx
@@ -1,4 +1,4 @@
-import {useRoute} from '@react-navigation/native';
+import {useNavigation, useRoute} from '@react-navigation/native';
 import type {ReactNode} from 'react';
 import React, {useEffect, useState} from 'react';
 import type {OnyxEntry} from 'react-native-onyx';
@@ -14,6 +14,7 @@ import Navigation from '@libs/Navigation/Navigation';
 import {isReportActionVisible, isWhisperAction} from '@libs/ReportActionsUtils';
 import {canUserPerformWriteAction} from '@libs/ReportUtils';
 import ONYXKEYS from '@src/ONYXKEYS';
+import ROUTES from '@src/ROUTES';
 import {getReportActionByIDSelector} from '@src/selectors/ReportAction';
 import {isLoadingInitialReportActionsSelector} from '@src/selectors/ReportMetaData';
 import type {ReportActions} from '@src/types/onyx';
@@ -52,6 +53,7 @@ function LinkedActionNotFoundGate({reportActionIDFromRoute, children}: LinkedAct
     const route = useRoute();
     const routeParams = route.params as {reportID?: string; reportActionID?: string} | undefined;
     const reportIDFromRoute = getNonEmptyStringOnyxID(routeParams?.reportID);
+    const navigation = useNavigation();
 
     const styles = useThemeStyles();
     const {accountID: currentUserAccountID} = useCurrentUserPersonalDetails();
@@ -162,13 +164,17 @@ function LinkedActionNotFoundGate({reportActionIDFromRoute, children}: LinkedAct
         Navigation.setParams({reportActionID: ''});
     };
 
+    // Just go back where we came from if there's navigation history. If there is no history, fallback to the report for
+    // this action.
+    const goBack = () => (navigation.canGoBack() ? Navigation.goBack() : Navigation.goBack(ROUTES.REPORT_WITH_ID.getRoute(reportIDFromRoute)));
+
     return (
         <FullPageNotFoundView
             shouldShow={shouldShowNotFoundLinkedAction}
             subtitleKey="notFound.commentYouLookingForCannotBeFound"
             subtitleStyle={[styles.textSupporting]}
             shouldShowBackButton={shouldUseNarrowLayout}
-            onBackButtonPress={navigateToEndOfReport}
+            onBackButtonPress={goBack}
             shouldShowLink
             linkTranslationKey="notFound.goToChatInstead"
             subtitleKeyBelowLink="notFound.contactConcierge"
