05435ff9328568c846d57e88127fc1107ece6f46
aswin-s <mr.aswin@gmail.com>
2026-05-03 11:37:22 +0530

    fix: remove unnecessary type import for React in SavedSearchList

--- src/pages/Search/SavedSearchList.tsx
+++ src/pages/Search/SavedSearchList.tsx
@@ -1,6 +1,6 @@
 import {useIsFocused} from '@react-navigation/native';
 import {accountIDSelector} from '@selectors/Session';
-import type React from 'react';
+import React from 'react';
 import MenuItemList from '@components/MenuItemList';
 import {usePersonalDetails} from '@components/OnyxListItemProvider';
 import {useProductTrainingContext} from '@components/ProductTrainingContext';
