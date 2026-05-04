3e5cdde3b9e772cc47d41a1cbae33b3abe82720f
Lukasz Modzelewski <lukasz.modzelewski@callstack.com>
2026-05-04 14:57:45 +0200

    reuse personalDetailsSelector in DelegateOnBehalfOfText

--- src/pages/inbox/report/DelegateOnBehalfOfText.tsx
+++ src/pages/inbox/report/DelegateOnBehalfOfText.tsx
@@ -1,12 +1,12 @@
+import {personalDetailsSelector} from '@selectors/PersonalDetails';
 import React from 'react';
-import type {OnyxEntry} from 'react-native-onyx';
 import Text from '@components/Text';
 import useLocalize from '@hooks/useLocalize';
 import useOnyx from '@hooks/useOnyx';
 import useThemeStyles from '@hooks/useThemeStyles';
 import {getPersonalDetailByEmail} from '@libs/PersonalDetailsUtils';
+import CONST from '@src/CONST';
 import ONYXKEYS from '@src/ONYXKEYS';
-import type * as OnyxTypes from '@src/types/onyx';
 
 type DelegateOnBehalfOfTextProps = {
     /** The account ID whose login drives the "on behalf of" name. */
@@ -19,9 +19,7 @@ type DelegateOnBehalfOfTextProps = {
 function DelegateOnBehalfOfText({mainAccountID, fallbackLogin}: DelegateOnBehalfOfTextProps) {
     const styles = useThemeStyles();
     const {translate} = useLocalize();
-    const [resolvedDetail] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {
-        selector: (list: OnyxEntry<OnyxTypes.PersonalDetailsList>) => (mainAccountID ? list?.[mainAccountID] : undefined),
-    });
+    const [resolvedDetail] = useOnyx(ONYXKEYS.PERSONAL_DETAILS_LIST, {selector: personalDetailsSelector(mainAccountID ?? CONST.DEFAULT_NUMBER_ID)});
     const mainAccountLogin = resolvedDetail?.login ?? fallbackLogin;
     const accountOwnerDetails = getPersonalDetailByEmail(String(mainAccountLogin ?? ''));
     return <Text style={[styles.chatDelegateMessage]}>{translate('delegate.onBehalfOfMessage', accountOwnerDetails?.displayName ?? '')}</Text>;
