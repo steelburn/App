14a89fb4f0318876104094082b5363c489c6ee62
Manan Jadhav <manan.jadhav@gmail.com>
2026-05-06 22:31:59 +0530

    fix: rollback unwanted change

--- src/libs/Navigation/helpers/goBackFromExportConnection.ts
+++ src/libs/Navigation/helpers/goBackFromExportConnection.ts
@@ -1,21 +1,18 @@
 import Navigation from '@libs/Navigation/Navigation';
 import CONST from '@src/CONST';
-import type {Route} from '@src/ROUTES';
 import replaceCompanyCardsRoute from './replaceCompanyCardsRoute';
 
 /**
  * If export company card value is changed to unsupported - we should redirect user directly to card details view
  * If not, just regular go back
  */
-function goBackFromExportConnection(shouldGoBackToSpecificRoute: boolean, backTo?: string, dynamicBackPath?: Route) {
+function goBackFromExportConnection(shouldGoBackToSpecificRoute: boolean, backTo?: string) {
     const feature = CONST.UPGRADE_FEATURE_INTRO_MAPPING.companyCards;
-    const effectivePath = dynamicBackPath ?? backTo;
 
-    // TODO: Remove `backTo` fallback once all routes using this helper are migrated to dynamic back paths.
-    if (!(shouldGoBackToSpecificRoute && effectivePath?.includes(feature.alias))) {
-        return Navigation.goBack(dynamicBackPath);
+    if (!(shouldGoBackToSpecificRoute && backTo?.includes(feature.alias))) {
+        return Navigation.goBack();
     }
-    const companyCardDetailsPage = replaceCompanyCardsRoute(effectivePath);
+    const companyCardDetailsPage = replaceCompanyCardsRoute(backTo);
     return Navigation.goBack(companyCardDetailsPage, {compareParams: false});
 }
 
