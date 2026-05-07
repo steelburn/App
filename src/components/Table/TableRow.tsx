914e54f2879056aeed6ea4cb0d144d4e80bb4e98
Jack Senyitko <jsenyitko@expensify.com>
2026-05-07 10:26:27 -0400

    add table row component

--- src/components/Table/TableRow.tsx
+++ src/components/Table/TableRow.tsx
@@ -1,5 +1,39 @@
-type TableRowProps = {};
+import PressableWithFeedback, {PressableWithFeedbackProps} from '@components/Pressable/PressableWithFeedback';
+import useThemeStyles from '@hooks/useThemeStyles';
+import CONST from '@src/CONST';
 
-export default function TableRow({}: TableRowProps) {
-    return <></>;
+type TableRowProps = PressableWithFeedbackProps & {
+    /** Whether this row is the last row in the table */
+    isLastRow?: boolean;
+};
+
+export default function TableRow({children, isLastRow, onPress, ...props}: TableRowProps) {
+    const styles = useThemeStyles();
+    const isPressable = onPress !== undefined;
+
+    const tableRowStyles = [
+        styles.mh5,
+        styles.flexRow,
+        styles.highlightBG,
+        styles.overflowHidden,
+        styles.ph3,
+        styles.tableRowHeight,
+        styles.alignItemsCenter,
+        isLastRow ? styles.tableBottomRadius : styles.borderBottom,
+    ];
+
+    return (
+        <PressableWithFeedback
+            accessibilityLabel="row"
+            style={tableRowStyles}
+            interactive={isPressable}
+            pressDimmingValue={isPressable ? undefined : 1}
+            hoverStyle={isPressable && styles.hoveredComponentBG}
+            role={isPressable ? CONST.ROLE.BUTTON : CONST.ROLE.PRESENTATION}
+            onPress={onPress}
+            {...props}
+        >
+            {children}
+        </PressableWithFeedback>
+    );
 }
