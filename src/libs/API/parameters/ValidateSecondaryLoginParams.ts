f92dbea279a058fd53fa41db9f33ae21d8e7ca97
Bernhard Owen Josephus <bernhard.josephus@gmail.com>
2026-05-12 11:04:32 +0800

    prettier

--- src/libs/API/parameters/ValidateSecondaryLoginParams.ts
+++ src/libs/API/parameters/ValidateSecondaryLoginParams.ts
@@ -1,3 +1,3 @@
-type ValidateSecondaryLoginParams = {partnerUserID: string; validateCode: string, deviceInfo: string};
+type ValidateSecondaryLoginParams = {partnerUserID: string; validateCode: string; deviceInfo: string};
 
 export default ValidateSecondaryLoginParams;
