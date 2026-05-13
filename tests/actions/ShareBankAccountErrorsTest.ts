78c74a3a4448012d084e5fe348478ea9b1e0adc5
Fedi Rajhi <fedirjh@gmail.com>
2026-05-13 21:07:54 +0100

    fix: use getOnyxValue helper and correct assertions in ShareBankAccountErrorsTest

--- tests/actions/ShareBankAccountErrorsTest.ts
+++ tests/actions/ShareBankAccountErrorsTest.ts
@@ -1,6 +1,7 @@
 import Onyx from 'react-native-onyx';
 import {clearShareBankAccountErrors} from '@libs/actions/BankAccounts';
 import ONYXKEYS from '@src/ONYXKEYS';
+import getOnyxValue from '../utils/getOnyxValue';
 import waitForBatchedUpdates from '../utils/waitForBatchedUpdates';
 
 describe('clearShareBankAccountErrors', () => {
@@ -16,30 +17,25 @@ describe('clearShareBankAccountErrors', () => {
 
     it('should clear SHARE_BANK_ACCOUNT errors when called without bankAccountID', async () => {
         await Onyx.merge(ONYXKEYS.SHARE_BANK_ACCOUNT, {
-            errors: {123456789: 'Some error'},
+            errors: {[Date.now()]: 'Some error'},
         });
         await waitForBatchedUpdates();
 
         clearShareBankAccountErrors();
         await waitForBatchedUpdates();
 
-        const connection = Onyx.connect({
-            key: ONYXKEYS.SHARE_BANK_ACCOUNT,
-            callback: (value) => {
-                Onyx.disconnect(connection);
-                expect(value?.errors).toBeNull();
-            },
-        });
+        const value = await getOnyxValue(ONYXKEYS.SHARE_BANK_ACCOUNT);
+        expect(value?.errors).toBeUndefined();
     });
 
     it('should clear both SHARE_BANK_ACCOUNT and BANK_ACCOUNT_LIST errors when called with bankAccountID', async () => {
-        const bankAccountID = 9053259;
+        const bankAccountID = 9053260;
         await Onyx.merge(ONYXKEYS.SHARE_BANK_ACCOUNT, {
-            errors: {123456789: 'Some error'},
+            errors: {[Date.now()]: 'Some error'},
         });
         await Onyx.merge(ONYXKEYS.BANK_ACCOUNT_LIST, {
             [bankAccountID]: {
-                errors: {123456789: 'Auth ShareBankAccount returned an error'},
+                errors: {[Date.now()]: 'Auth ShareBankAccount returned an error'},
             },
         });
         await waitForBatchedUpdates();
@@ -47,28 +43,18 @@ describe('clearShareBankAccountErrors', () => {
         clearShareBankAccountErrors(bankAccountID);
         await waitForBatchedUpdates();
 
-        const shareBankAccountConnection = Onyx.connect({
-            key: ONYXKEYS.SHARE_BANK_ACCOUNT,
-            callback: (value) => {
-                Onyx.disconnect(shareBankAccountConnection);
-                expect(value?.errors).toBeNull();
-            },
-        });
+        const shareBankAccountValue = await getOnyxValue(ONYXKEYS.SHARE_BANK_ACCOUNT);
+        expect(shareBankAccountValue?.errors).toBeUndefined();
 
-        const bankAccountListConnection = Onyx.connect({
-            key: ONYXKEYS.BANK_ACCOUNT_LIST,
-            callback: (value) => {
-                Onyx.disconnect(bankAccountListConnection);
-                expect(value?.[bankAccountID]?.errors).toBeNull();
-            },
-        });
+        const bankAccountListValue = await getOnyxValue(ONYXKEYS.BANK_ACCOUNT_LIST);
+        expect(bankAccountListValue?.[bankAccountID]?.errors).toBeUndefined();
     });
 
     it('should not modify BANK_ACCOUNT_LIST when called without bankAccountID', async () => {
-        const bankAccountID = 9053259;
-        const existingErrors = {123456789: 'Auth ShareBankAccount returned an error'};
+        const bankAccountID = 9053261;
+        const existingErrors = {[Date.now()]: 'Auth ShareBankAccount returned an error'};
         await Onyx.merge(ONYXKEYS.SHARE_BANK_ACCOUNT, {
-            errors: {123456789: 'Some error'},
+            errors: {[Date.now()]: 'Some error'},
         });
         await Onyx.merge(ONYXKEYS.BANK_ACCOUNT_LIST, {
             [bankAccountID]: {errors: existingErrors},
@@ -78,12 +64,7 @@ describe('clearShareBankAccountErrors', () => {
         clearShareBankAccountErrors();
         await waitForBatchedUpdates();
 
-        const connection = Onyx.connect({
-            key: ONYXKEYS.BANK_ACCOUNT_LIST,
-            callback: (value) => {
-                Onyx.disconnect(connection);
-                expect(value?.[bankAccountID]?.errors).toEqual(existingErrors);
-            },
-        });
+        const value = await getOnyxValue(ONYXKEYS.BANK_ACCOUNT_LIST);
+        expect(value?.[bankAccountID]?.errors).toEqual(existingErrors);
     });
 });
