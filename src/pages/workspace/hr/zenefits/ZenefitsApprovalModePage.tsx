c0cb397d7e4f347b44cf6f5fe61a57e7bea93d3c
krishna2323 <belivethatkg@gmail.com>
2026-05-15 01:07:21 +0530

    fix: Remove commented-out code from Zenefits stub pages
    Signed-off-by: krishna2323 <belivethatkg@gmail.com>

--- src/pages/workspace/hr/zenefits/ZenefitsApprovalModePage.tsx
+++ src/pages/workspace/hr/zenefits/ZenefitsApprovalModePage.tsx
@@ -4,12 +4,6 @@ import ScreenWrapper from '@components/ScreenWrapper';
 import useLocalize from '@hooks/useLocalize';
 import Navigation from '@libs/Navigation/Navigation';
 
-// import type {PlatformStackScreenProps} from '@libs/Navigation/PlatformStackNavigation/types';
-// import type {SettingsNavigatorParamList} from '@libs/Navigation/types';
-// import type SCREENS from '@src/SCREENS';
-
-// type ZenefitsApprovalModePageProps = PlatformStackScreenProps<SettingsNavigatorParamList, typeof SCREENS.WORKSPACE.HR_ZENEFITS_APPROVAL_MODE>;
-
 function ZenefitsApprovalModePage() {
     const {translate} = useLocalize();
 
